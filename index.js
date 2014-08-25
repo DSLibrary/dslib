5509484170a398104be58b5cfd2996c9f2f1ddb3
Kia Fathi <kiafathi@me.com>
2014-08-24 17:26:05 -0700

    [FEATURE] Add set to index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 var Stack = require('./dataStructures/stack.js');
 var Queue = require('./dataStructures/queue.js');
 var LinkedList = require('./dataStructures/linkedList.js');
+var Set = require('./dataStructures/set.js');
 
 //library object that contains all data structures
 var dslib = {};
 dslib.Stack = Stack;
 dslib.Queue = Queue;
 dslib.LinkedList = LinkedList;
+dslib.Set = Set;
 
 module.exports = dslib;
