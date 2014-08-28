d6a82d1757de2dd6e405c27cf6cdc41b35b8f6db
Kia Fathi <kiafathi@me.com>
2014-08-28 12:14:26 -0700

    [FEATURE] Begin bloomFilter implementation

--- dataStructures/bloomFilter.js
+++ dataStructures/bloomFilter.js
@@ -1,5 +1,30 @@
 'use strict';
 
-var BloomFilter = function(){};
+var BloomFilter = function(space, hashes){
+  this._storage = new Array(space);
+  this._size = 0;
+};
+
+BloomFilter.prototype.add = function(key){
+  var h1key = hashingFunction1(key);
+  var h2key = hashingFunction2(key);
+  this._storage[h1key] = true;
+  this._storage[h2key] = true;
+  this._size++;
+};
+
+BloomFilter.prototype.test = function(key){
+  var h1key = hashingFunction1(key);
+  var h2key = hashingFunction2(key);
+  return !!this._storage[h1key] && !!this._storage[h2key];
+};
+
+function hashingFunction1(key){
+  return !!key%2 ? 1 : 2;
+}
+
+function hashingFunction2(key){
+  return key % 3;
+}
 
 module.exports = BloomFilter;
--- tests/bloomFilterSpec.js
+++ tests/bloomFilterSpec.js
@@ -3,7 +3,42 @@ var should = require('chai').should();
 var BloomFilter = require('../index.js').BloomFilter;
 
 describe('BloomFilter', function(){
+
+  var exBF;
+  beforeEach(function(){
+    exBF = new BloomFilter(3);
+  });
+
   it('should be a function', function(){
     (typeof BloomFilter).should.equal('function');
   });
+
+  it('should have an add method', function(){
+    (typeof exBF.add).should.equal('function');
+  });
+
+  it('should have a test method', function(){
+    (typeof exBF.test).should.equal('function');
+    exBF.add(1);
+    exBF.add(2);
+    (exBF.test(1)).should.equal(true);
+    (exBF.test(2)).should.equal(true);
+    (exBF.test(3)).should.equal(false);
+  });
+
+  it('should be able to function with multiple different hash sizes', function(){
+    var BF1 = new BloomFilter(15, 1);
+    var BF2 = new BloomFilter(14, 2);
+    BF1.add(1);
+    BF1.add(2);
+    (BF1.test(1)).should.equal(true);
+    (BF1.test(2)).should.equal(true);
+    (BF1.test(3)).should.equal(false);
+    BF2.add(1);
+    BF2.add(2);
+    (BF2.test(1)).should.equal(true);
+    (BF2.test(2)).should.equal(true);
+    (BF2.test(3)).should.equal(false);
+  });
+
 });
