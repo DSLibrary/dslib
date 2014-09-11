d15d164f415efe04cdd43e33ea127cdd5f09d489
David Deriso <davidderiso@gmail.com>
2014-09-10 19:59:02 -0700

    [FEATURE] Added new methods to Set. Added new methods to Set:   clear() - removes all contents.   delete() - synonym for remove().   has() - synonym for contains().   size() - returns count of items in the set.

--- dataStructures/set.js
+++ dataStructures/set.js
@@ -5,15 +5,36 @@ var Set = function(){
 };
 
 Set.prototype.add = function(value){
-  this._storage[value] = true;
+  this._storage[value] = value;
 };
 
 Set.prototype.remove = function(value){
-  this._storage[value] = false;
+  delete this._storage[value];
+};
+
+// Same as remove function.
+Set.prototype.delete = function(value){
+  this.remove(value);
 };
 
 Set.prototype.contains = function(value){
-  return this._storage[value];
+  //return (this._storage[value] === true);
+  return (this._storage.hasOwnProperty(value));
 };
+
+// Clears the Set of all values.
+Set.prototype.clear = function(){
+  this._storage = {};
+};
+
+// Returns the count of items in this set.
+Set.prototype.size = function(){
+  return Object.keys(this._storage).length;
+}
+
+// Same as contains function.
+Set.prototype.has = function(value){
+  return this.contains(value);
+}
 
 module.exports = Set;
