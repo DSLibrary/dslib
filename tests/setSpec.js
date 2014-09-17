448fa3dbfdb27e1f929313288f33abe99f236dee
Peter Hayes <peter.k.hayes@gmail.com>
2014-09-16 23:58:59 -0700

    added union, intersection, difference, toArray, fromArray, and copy methods to set.  changed set init to use Object.create(null)

--- tests/setSpec.js
+++ tests/setSpec.js
@@ -3,8 +3,6 @@
 var Set = require('../index.js').Set;
 var should = require('chai').should();
 
-
-
 describe('Set', function(){
 
   var exSet;
@@ -82,6 +80,58 @@ describe('Set', function(){
     (exSet.has(2)).should.equal(true);
     (exSet.has(1)).should.equal(false);
     (exSet.has(3)).should.equal(false);
+  });
+
+  it("should have a toArray method", function() {
+    (typeof exSet.toArray).should.equal('function');
+    var arr = exSet.toArray();
+    (arr.length).should.equal(2);
+    arr.should.contain(2);
+    arr.should.contain("Hello");
+  });
+
+  it("should have a fromArray method", function() {
+    (typeof Set.fromArray).should.equal('function');
+    var set = Set.fromArray(["1", 2, 3]);
+    (set.has("1")).should.equal(true);
+    (set.has(2)).should.equal(true);
+    (set.has(3)).should.equal(true);
+  });
+
+  it("should have a copy method", function() {
+    (typeof exSet.copy).should.equal('function');
+    var newSet = exSet.copy();
+    (newSet.has("Hello")).should.equal(true);
+    (newSet.has(2)).should.equal(true);
+    exSet.add(1);
+    (newSet.has(1)).should.equal(false);
+  });
+
+  it("should have a union method", function() {
+    (typeof exSet.union).should.equal('function');
+    var set1 = Set.fromArray([1, 2, 3]);
+    var set2 = Set.fromArray([3, 4, 5, 6]);
+    var set3 = Set.fromArray([1, 3, 7]);
+    var union = set1.union(set2, set3);
+    (union.toArray().sort()).should.deep.equal([1, 2, 3, 4, 5, 6, 7]);
+  });
+
+  it("should have a difference method", function() {
+    (typeof exSet.difference).should.equal('function');
+    var set1 = Set.fromArray([0, 1, 2, 3]);
+    var set2 = Set.fromArray([3, 4, 5, 6]);
+    var set3 = Set.fromArray([1, 3, 7]);
+    var difference = set1.difference(set2, set3);
+    difference.toArray().sort().should.deep.equal([0, 2]);
+  });
+
+  it("should have an intersection method", function() {
+    (typeof exSet.intersection).should.equal('function');
+    var set1 = Set.fromArray([1, 2, 3, 4]);
+    var set2 = Set.fromArray([3, 4, 5, 6]);
+    var set3 = Set.fromArray([1, 3, 7, 4]);
+    var intersection = set1.intersection(set2, set3);
+    intersection.toArray().sort().should.deep.equal([3, 4]);
   });
 
 });
