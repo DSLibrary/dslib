f9b799bfb64f6e13b28ede2b9fafea4844ae8ef6
forrestbthomas <forrestbthomas@gmail.com>
2014-09-26 12:26:21 -0700

    [FEATURE]Add more tests

--- tests/DEQueueSpec.js
+++ tests/DEQueueSpec.js
@@ -14,32 +14,44 @@ describe('DEQueue', function(){
 
   it('should have an addFront method', function(){
     (typeof exDEQueue.addFront).should.equal('function');
-    exDEQueue.addFront(1);
-    exDEQueue.addFront(2);
   });
 
   it('should have an addRear method', function(){
     (typeof exDEQueue.addRear).should.equal('function');
-    exDEQueue.addRear(1);
-    exDEQueue.addRear(2);
   });
 
   it('should have a removeFront method', function(){
     (typeof exDEQueue.removeFront).should.equal('function');
-    exDEQueue.removeFront(1);
-    exDEQueue.removeFront(2);
+  });
+
+  it('should return the front value when removeFront is called', function(){
+    exDEQueue.addFront(3);
+    exDEQueue.addFront(2);
+    exDEQueue.addFront(1);
+    (typeof exDEQueue.removeFront()).should.equal(1);
+  });
+
+  it('should not remove a value, if the DEQueue is empty', function(){
+    (exDEQueue.removeFront()).should.equal(undefined);
   });
 
   it('should have a removeRear method', function(){
     (typeof exDEQueue.removeRear).should.equal('function');
-    exDEQueue.removeRear(1);
-    exDEQueue.removeRear(2);
+  });
+
+  it('should return the rear value when removeRear is called', function(){
+    exDEQueue.addFront(3);
+    exDEQueue.addFront(2);
+    exDEQueue.addFront(1);
+    (typeof exDEQueue.removeRear()).should.equal(3);
+  });
+
+  it('should not remove a value, if the DEQueue is empty', function(){
+    (exDEQueue.removeRear()).should.equal(undefined);
   });
 
   it('should have an isEmpty method', function(){
     (typeof exDEQueue.isEmpty).should.equal('function');
-    exDEQueue.isEmpty(1);
-    exDEQueue.isEmpty(2);
   });
 
   it('should have a functioning size method', function(){
